3caac53edc172a70b507a680f36b5557f78478cf
Jeff Mitsou <shinetachi88@gmail.com>
2021-08-12 22:10:02 -0400

    completed the assignment

--- src/com/company/CLI.java
+++ src/com/company/CLI.java
@@ -1,32 +1,34 @@
 package com.company;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class CLI {
 
     public void menuSelection() {
         Scanner userConvo = new Scanner(System.in);
-        System.out.println("Please choose an option to continue:");
+        System.out.println("\nPlease choose an option to continue:");
         System.out.println("(1) Say Hello \n(2) Reverse a string \n(3) Add two numbers \n(4) Exit Program");
-        System.out.println("Selection: ");
-
+        System.out.print("Selection: ");
         int enteredValue = userConvo.nextInt();
 
         if (enteredValue == 1) {
-            System.out.println("What is your name: ");
+            System.out.print("What is your name: ");
+            userConvo.nextLine();
             String userName = userConvo.nextLine();
             System.out.println("Hello " + userName);
-            menuSelection();
 
+            proceed();
         } else if (enteredValue == 2) {
 
             StringBuilder str = new StringBuilder();
             System.out.println("Please enter a string to reverse: ");
+            userConvo.nextLine();
             String entStr = userConvo.nextLine();
             str.append(entStr);
             str.reverse();
             System.out.println(str);
-            menuSelection();
+            proceed();
 
         } else if (enteredValue == 3) {
 
@@ -36,12 +38,23 @@ public class CLI {
             int val2 = userConvo.nextInt();
             int sum = val1 + val2;
             System.out.println("Total value is " + sum);
-            menuSelection();
+
+            proceed();
 
         } else if (enteredValue == 4) {
-
             System.out.println("Bye have a nice day");
         }
+    }
 
+    public void proceed() {
+        Scanner pro = new Scanner(System.in);
+        System.out.println("Would you like to choose another option? (1) Yes or (2) No");
+        int choice = pro.nextInt();
+
+        if (choice == 1) {
+            menuSelection();
+        } else {
+            System.out.println("Bye have a nice day");
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Gabe's CLI!\n");
+        System.out.println("Welcome to Jeff's CLI!");
         CLI input = new CLI();
         input.menuSelection();
     }
